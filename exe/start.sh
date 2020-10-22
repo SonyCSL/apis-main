ca9f38b01b906e9f1626cd189a7557b2d44455f8
岩瀬俊一郎 <iwaseshunichirou@iwaseshunichirounoMacBook-puro.local>
2020-10-22 10:06:37 +0900

    Add newly exe/.

---
+++ exe/start.sh
@@ -0,0 +1,5 @@
+echo "call start.sh"
+
+java -Djava.net.preferIPv4Stack=true -Duser.timezone=Asia/Tokyo -Djava.util.logging.config.file=./logging.properties -jar ../target/apis-main-3.0.0-a01-fat.jar -conf ./config.json -cp ./ -cluster  -cluster-host 127.0.0.1
+
+echo "... done"
