708b8b613b7edc5ca7f30606bc3d3e691200422b
岩瀬俊一郎 <iwaseshunichirou@iwaseshunichirounoMacBook-puro.local>
2020-10-22 22:17:38 +0900

    Change src/ , exe/, pom.xml

--- exe/start3.sh
+++ exe/start3.sh
@@ -1,5 +1,5 @@
 echo "call start.sh"
 
-java -Djava.net.preferIPv4Stack=true -Duser.timezone=Asia/Tokyo -Djava.util.logging.config.file=./logging.properties -jar ../target/apis-main-3.0.0-a01-fat.jar -conf ./config3.json -cp ./ -cluster  -cluster-host 127.0.0.1
+java -Djava.net.preferIPv4Stack=true -Duser.timezone=Asia/Tokyo -Djava.util.logging.config.file=./logging.properties -jar ../target/apis-main-3.0.0-fat.jar -conf ./config3.json -cp ./ -cluster  -cluster-host 127.0.0.1
 
 echo "... done"
--- src/main/resources/stop-kill.sh
+++
@@ -1,7 +0,0 @@
-echo "call stop-kill.sh"
-
-timeout -sKILL 300 sh stop.sh
-if [ $? != 0 ]; then
- echo timed out
- sh kill.sh
-fi
