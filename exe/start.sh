dbc6278c172d8e6dc67a974c936a4553e43de707
岩瀬俊一郎 <iwaseshunichirou@iwaseshunichirounoMacBook-puro.local>
2020-10-23 17:44:44 +0900

    Add pom.xml, exe/, src/.

--- exe/start.sh
+++ exe/start.sh
@@ -1,5 +1,10 @@
 echo "call start.sh"
 
-java -Djava.net.preferIPv4Stack=true -Duser.timezone=Asia/Tokyo -Djava.util.logging.config.file=./logging.properties -jar ../target/apis-main-3.0.0-fat.jar -conf ./config.json -cp ./ -cluster  -cluster-host 127.0.0.1
+UNAME=$(uname)
+if [ "$UNAME" = 'Darwin' ] ; then
+	java -Djava.net.preferIPv4Stack=true -Duser.timezone=Asia/Tokyo -Djava.util.logging.config.file=./logging.properties -Dvertx.hazelcast.config=./cluster-mac.xml -jar ../target/apis-main-3.0.0-fat.jar -conf ./config.json -cluster -cluster-host 127.0.0.1
+else
+	java -Djava.net.preferIPv4Stack=true -Duser.timezone=Asia/Tokyo -Djava.util.logging.config.file=./logging.properties -Dvertx.hazelcast.config=./cluster.xml -jar ../target/apis-main-3.0.0-fat.jar -conf ./config.json -cluster -cluster-host 127.0.0.1
+fi
 
 echo "... done"
